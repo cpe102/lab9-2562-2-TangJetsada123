3f8175f7d5e221a9192269eed26ce71479bd4234
lab314-17@labeng.local <lab314-17@labeng.local>
2020-01-28 16:46:06 +0700

    Lab9_ver1

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,3 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ifstream  source;
+    ofstream dest("C:\\Users\\lab314-17\\Desktop\\New folder\\lab9-2562-2-TangJetsada123\\cheerbook_copy.txt");
+    source.open("C:\\Users\\lab314-17\\Desktop\\New folder\\lab9-2562-2-TangJetsada123\\cheerbook.txt");
+    string textline;
+    bool    havetext;
+    havetext = getline(source,textline);
+    dest<< "-------------------- SOTUS ---------------------"<<"\n";
+    while(havetext){
+        dest<< textline<<"\n";
+        havetext = getline(source,textline);
+    }
+        dest<< "-------------------- SOTUS ---------------------"<<"\n";
+        source.close();
+        dest.close();
+    return 0;
+
+}
+
+
+
+
 //cout << "-------------------- SOTUS ---------------------";
 //cout << textline;
 //cout << "-------------------- SOTUS ---------------------";
